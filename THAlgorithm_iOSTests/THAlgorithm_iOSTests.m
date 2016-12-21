1694b96724b9fe16f11f602ee4d183ac4593681d
Haoero <haoero@futunn.com>
2016-12-21 21:38:21 +0800

    revised

---
+++ THAlgorithm_iOSTests/THAlgorithm_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  THAlgorithm_iOSTests.m
+//  THAlgorithm_iOSTests
+//
+//  Created by TANG HAO on 21/12/16.
+//  Copyright © 2016年 HAOERO TANG. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface THAlgorithm_iOSTests : XCTestCase
+
+@end
+
+@implementation THAlgorithm_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
